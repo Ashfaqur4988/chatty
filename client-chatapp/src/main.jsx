2af1822773215a16b67ddb6aea6bd8df6e64e6d8
Ashfaqur Riaz <ashfakurriaz@gmail.com>
2024-10-03 23:31:54 +0530

    SocketContext.jsx fixed

--- client-chatapp/src/main.jsx
+++ client-chatapp/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
-import SocketContextProvider from "./context/socketContext.jsx";
+import SocketContextProvider from "./context/SocketContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
